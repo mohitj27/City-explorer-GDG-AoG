6db737c2fa0c1f3f2f40e4d4cbf25f121fed9838
jeevan-jp <jiwanprakash65@gmail.com>
2018-07-20 01:46:51 +0530

    Intent city name working well

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 var cities = fs.readFileSync('cityInfo.json');
-  cities = JSON.parse(cities);
+cities = JSON.parse(cities);
 // Import the Dialogflow module from the Actions on Google client library.
 const {dialogflow} = require('actions-on-google');
 
@@ -14,11 +14,15 @@ const app = dialogflow({debug: true});
 
 app.intent('city name', (conv, {city}) => {
 
-   for(var i=0;i<cities.length;i++){
-     if(cities[i].city === city)
-     conv.close(cities[i]);
+   let theCity;
+   for(var i=0; i<cities.length; i++) {
+     if(cities[i].name.toLocaleLowerCase() === city.toLocaleLowerCase()) {
+        theCity = cities[i];
+        break;
+     }
    }
-  // conv.close('We\'ll show you details about ' + city +  ' soon.');
+   
+   theCity ? conv.close(city + ' is in ' + theCity.state) : conv.close('We have not mapped this city yet.');
 
   // Complete your fulfillment logic and
   // send a response when the function is done executing
