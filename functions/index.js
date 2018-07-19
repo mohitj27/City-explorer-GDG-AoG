9edcee2bcf40f97a75ee0417f8f9adf01355d3de
mohit <mjain8156@gmail.com>
2018-07-20 01:18:08 +0530

    added saple json

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const fs = require('fs');
+var cities = fs.readFileSync('cityInfo.json');
+  cities = JSON.parse(cities);
 // Import the Dialogflow module from the Actions on Google client library.
 const {dialogflow} = require('actions-on-google');
 
@@ -10,7 +13,13 @@ const functions = require('firebase-functions');
 const app = dialogflow({debug: true});
 
 app.intent('city name', (conv, {city}) => {
-  conv.close('We\'ll show you details about ' + city +  ' soon.');
+
+   for(var i=0;i<cities.length;i++){
+     if(cities[i].city === city)
+     conv.close(cities[i]);
+   }
+  // conv.close('We\'ll show you details about ' + city +  ' soon.');
+
   // Complete your fulfillment logic and
   // send a response when the function is done executing
 });
